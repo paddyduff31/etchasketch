7f2af3bb1a2763865dfa7e27e6bf33a2b8894161
Paddy Duff <paddy.duff31@googlemail.com>
2020-06-28 17:20:45 +0100

    javascript color changer

--- javascript.js
+++ javascript.js
@@ -1,12 +1,23 @@
+//make a grid
 const container = document.getElementById("container");
 
-        function makeRows(rows, cols) {
-            container.style.setProperty('--grid-rows', rows);
-            container.style.setProperty('--grid-cols', cols);
-            for (c = 0; c < (rows * cols); c++) {
-                let cell = document.createElement("div");
-                container.appendChild(cell).className = "grid-item";
-            };
-        };
+makeRows(16, 16);
 
-        makeRows(16, 16);
+function makeRows(rows, cols) {
+        for (c = 0; c < (rows * cols); c++) {
+            let cell = document.createElement("div");
+            container.appendChild(cell).className = "grid-item";
+        };
+};
+
+
+//Make the grid squares blue when passing over them
+const gridItems = document.querySelectorAll("div.grid-item");
+
+gridItems.forEach((gridItems) => {
+  gridItems.addEventListener('mouseover', (e) => {
+    gridItems.style.backgroundColor = 'blue';
+  });
+});
+        
+
