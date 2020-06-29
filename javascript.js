895008cef27d39b4960cf1ba3ae51f2d5e4b59f7
Paddy Duff <paddy.duff31@googlemail.com>
2020-06-29 16:51:37 +0100

    new layout

--- javascript.js
+++ javascript.js
@@ -1,23 +1,56 @@
 //make a grid
 const container = document.getElementById("container");
+var gridSquares = 0;
+let eraserOn = 0;
+makeRows(16);
 
-makeRows(16, 16);
-
-function makeRows(rows, cols) {
-        for (c = 0; c < (rows * cols); c++) {
+function makeRows(gridSquares) {
+        container.style.setProperty('--grid-rows', gridSquares);
+        container.style.setProperty('--grid-cols', gridSquares);
+        for (c = 0; c < (gridSquares*gridSquares); c++) {
             let cell = document.createElement("div");
             container.appendChild(cell).className = "grid-item";
         };
 };
 
 
-//Make the grid squares blue when passing over them
-const gridItems = document.querySelectorAll("div.grid-item");
+//Make the grid squares black when passing over them
+var gridItems = document.querySelectorAll("div.grid-item");
 
 gridItems.forEach((gridItems) => {
   gridItems.addEventListener('mouseover', (e) => {
-    gridItems.style.backgroundColor = 'blue';
+    gridItems.style.backgroundColor = 'black';
   });
 });
-        
+
+
+ 
+
+function clearGrid() {
+    gridItems.forEach((gridItems) => {
+        gridItems.style.backgroundColor = 'transparent';
+    });
+}
+
+const clearButton = document.querySelector("#clear-button")
+clearButton.addEventListener('click', clearGrid);
+
+
+const gridSize = document.querySelector("#grid-size")
+
+gridSize.addEventListener('click', (e) => {
+    clearGrid();
+    let promptAnswer = prompt('Grid size?');
+    makeRows(promptAnswer);
+});
+
+
+const eraser = document.querySelector("#eraser")
+
+eraser.addEventListener("click", () => {
+    gridItems.forEach((square) => {
+        square.onmouseover = () => square.removeAttribute("style");
+    });
+})
+
 
